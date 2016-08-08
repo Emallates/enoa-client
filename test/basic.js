d450ef95fd814db2592e9d36ce370df60b25e80f
Nauman Ramzan <nomi_ramzan@yahoo.com>
2016-08-08 17:04:36 +0400

    Added mocha tests

---
+++ test/basic.js
@@ -0,0 +1,16 @@
+var HttpConnector = require('../lib/connectors/http.js');
+var exec = require('easy-mocha')
+var client = require('../')
+var defConf = { appId:'client-appId', apiKey:'client-apiKey'};
+/* Simple Example*/
+
+var tests = {
+	ttl:"Enoa-client Server side tests",
+	cases:[
+		{ttl:'Create client without adapter', opr:'equal', create:function(){ return client(defConf); }, rslt:1, cmp:function(clientObj, model){ return clientObj instanceof client; }},
+		{ttl:'Validate default configs', opr:'equal', create:function(){ return client(defConf); }, rslt:1, cmp:function(clientObj, model){ return clientObj.configs.appId == defConf.appId && clientObj.configs.apiKey == defConf.apiKey ; }},
+		{ttl:'Validate connector', opr:'equal', create:function(){ return client(defConf); }, rslt:1, cmp:function(clientObj, model){ return clientObj.configs.connector instanceof HttpConnector; }},
+	]
+}
+
+exec(tests)
